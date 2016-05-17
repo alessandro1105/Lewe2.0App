8ca3dedfbeb6ef72f0d096747c6e52cc56dbb840
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2016-05-17 18:57:06 +0200

    Sistemata activity Search e iniziato sviluppo MainActivity

--- app/src/main/java/com/lewetechnologies/app/SearchActivity.java
+++ app/src/main/java/com/lewetechnologies/app/SearchActivity.java
@@ -1,19 +1,37 @@
 package com.lewetechnologies.app;
 
-import android.support.v4.app.NavUtils;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_search);
 
-        //visualizzo il back button
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*
+        //stop search
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+        progress.setIndeterminateDrawable(null);
+
+        //change image icon
+        ImageView image = (ImageView) findViewById(R.id.icon);
+        image.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_error));
+        */
+
+
     }
 
 
