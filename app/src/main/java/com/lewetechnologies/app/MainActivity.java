286cce13831e9bc4e5912abe393fbe5fb381934c
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2016-05-18 11:49:15 +0200

    Sistemati i fragment della MainActivity e animata la bottombar

--- app/src/main/java/com/lewetechnologies/app/MainActivity.java
+++ app/src/main/java/com/lewetechnologies/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.lewetechnologies.app;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -22,15 +26,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * The {@link PagerAdapter} that will provide
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
      * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     * {@link FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
@@ -38,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,15 +80,28 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        //onclick bottom menu
+        //inizializzo la bottom bar
+        ((ImageView) findViewById(R.id.bottombar_icon_temperature)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_temperature_selected));
+        ((TextView) findViewById(R.id.bottombar_text_temperature)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color_selected));
+        ((ImageView) findViewById(R.id.bottombar_icon_gsr)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_gsr));
+        ((TextView) findViewById(R.id.bottombar_text_gsr)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color));
+
+        //onclick bottom bar
         //temperature
         ((RelativeLayout) findViewById(R.id.bottombar_action_temperature)).setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
 
+                //setto la nuova icona e il colore del testo della bottombar dell'elemento selezionato
                 ((ImageView) findViewById(R.id.bottombar_icon_temperature)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_temperature_selected));
+                ((TextView) findViewById(R.id.bottombar_text_temperature)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color_selected));
 
-                Toast.makeText(MainActivity.this.getApplicationContext(), "Temperature", Toast.LENGTH_LONG).show();
+                //resetto la bottombar
+                //setto la nuova icona e il colore del testo della bottombar dell'elemento selezionato
+                ((ImageView) findViewById(R.id.bottombar_icon_gsr)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_gsr));
+                ((TextView) findViewById(R.id.bottombar_text_gsr)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color));
+
+                mViewPager.setCurrentItem(0, true);
             }
 
         });
@@ -82,7 +110,16 @@ public class MainActivity extends AppCompatActivity {
         ((RelativeLayout) findViewById(R.id.bottombar_action_gsr)).setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this.getApplicationContext(), "GSR", Toast.LENGTH_LONG).show();
+
+                //setto la nuova icona e il colore del testo della bottombar dell'elemento selezionato
+                ((ImageView) findViewById(R.id.bottombar_icon_gsr)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_gsr_selected));
+                ((TextView) findViewById(R.id.bottombar_text_gsr)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color_selected));
+
+                //resetto la bottombar
+                //setto la nuova icona e il colore del testo della bottombar dell'elemento selezionato
+                ((ImageView) findViewById(R.id.bottombar_icon_temperature)).setImageDrawable(MainActivity.this.getDrawable(R.drawable.ic_temperature));
+                ((TextView) findViewById(R.id.bottombar_text_temperature)).setTextColor(ContextCompat.getColor(getApplication(), R.color.activity_main_bottombar_element_description_text_color));
+                mViewPager.setCurrentItem(1, true);
             }
 
         });
@@ -97,6 +134,9 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -164,42 +204,89 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+    @Override
+    public void onStart() {
+        super.onStart();
 
-        public PlaceholderFragment() {
-        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.lewetechnologies.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.lewetechnologies.app/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    public static class TemperatureFragment extends Fragment {
+
+        private View rootView;
+
+        public TemperatureFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false); //TEMPERATURE
 
-            Typeface blockFonts = Typeface.createFromAsset(PlaceholderFragment.this.getContext().getAssets(),"fonts/DINCond-Bold.ttf");
+            //creo la vista
+            rootView = inflater.inflate(R.layout.fragment_main_temperature, container, false); //TEMPERATURE
 
+            //cambio il font del valore mostrato
+            Typeface blockFonts = Typeface.createFromAsset(TemperatureFragment.this.getContext().getAssets(), "fonts/DINCond-Bold.ttf");
             ((TextView) rootView.findViewById(R.id.number)).setTypeface(blockFonts);
-            //View rootView = inflater.inflate(R.layout.gsr_layout, container, false); //GSR
-            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+
+            //ritorno la vista
+            return rootView;
+        }
+    }
+
+    public static class GSRFragment extends Fragment {
+
+        private View rootView;
+
+        public GSRFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            //creo la vista
+            rootView = inflater.inflate(R.layout.fragment_main_gsr, container, false); //TEMPERATURE
+
+            //cambio il font del valore mostrato
+            Typeface blockFonts = Typeface.createFromAsset(GSRFragment.this.getContext().getAssets(), "fonts/DINCond-Bold.ttf");
+            ((TextView) rootView.findViewById(R.id.number)).setTypeface(blockFonts);
+
+            //ritorno la vista
             return rootView;
         }
     }
@@ -210,34 +297,42 @@ public class MainActivity extends AppCompatActivity {
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
+        private TemperatureFragment temperatureFragment;
+        private GSRFragment gsrFragment;
+
+
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
+
+            //costruisco i fragment
+            temperatureFragment = new TemperatureFragment();
+            gsrFragment = new GSRFragment();
         }
 
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position) {
+
+                //Temperature fragment
+                case 0:
+                    return temperatureFragment; //ritorno il fragment della temperatura
+
+                //GSR fragment
+                case 1:
+                    return gsrFragment; //ritorno il fragment del GSR
+            }
+
+            //ritorno il fragment della temperatura nel caso di errore
+            return temperatureFragment;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
+            // Show 2 total pages.
+            return 2; //due fragment
         }
     }
+
 }
