77350ace5541de0aa7b60404b5b49b206e78f558
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2016-05-21 00:02:50 +0200

    Riscritta classe JData

--- app/src/main/java/com/lewetechnologies/app/jack/JData.java
+++ app/src/main/java/com/lewetechnologies/app/jack/JData.java
@@ -1,5 +1,7 @@
 package com.lewetechnologies.app.jack;
 
+import com.lewetechnologies.app.logger.Logger;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -12,7 +14,7 @@ public class JData {
     private JSONObject root;
 
     //nested object
-    private JSONObject value;
+    private JSONObject values;
 
     //indica se è stato
     private boolean nestedObjectExists;
@@ -35,44 +37,258 @@ public class JData {
         root = message;
 
         //prelevo il payload dei dati
-        value = root.getJSONObject(Jack.JK_MESSAGE_PAYLOAD);
+        values = root.getJSONObject(Jack.JK_MESSAGE_PAYLOAD);
 
     }
 
     //adder
-    public void add(String key, boolean value) {
+    public boolean add(String key, boolean value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, float value) {
+    public boolean add(String key, float value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, double value) {
+    public boolean add(String key, double value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, char value) {
+    public boolean add(String key, char value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
-    public void add(String key, long value) {
+
+    public boolean add(String key, long value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, int value) {
+    public boolean add(String key, int value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, short value) {
+    public boolean add(String key, short value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
 
-    public void add(String key, String value) {
+    public boolean add(String key, String value) {
+
+        //se non esiste l'oggetto nested lo creo
+        if (!nestedObjectExists) {
+            createNestedObject();
+        }
+
+        try {
+            values.put(key, value);
+
+            return true;
+
+        } catch (JSONException e) {
+            Logger.e("JData", "add", e, Logger.SEVERE);
+
+            return false;
+        }
 
     }
+
+    //get method
+
+    public boolean getBoolean(String key) {
+        try {
+            return values.getBoolean(key);
+
+        } catch (JSONException e) {
+            return false;
+        }
+    }
+
+    public double getFloat(String key) {
+        try {
+            return values.getDouble(key);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public double getDouble(String key) {
+        try {
+            return values.getDouble(key);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public char getChar(String key) {
+        try {
+            return values.getString(key).charAt(0);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public long getLong(String key) {
+        try {
+            return values.getLong(key);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public int getInt(String key) {
+        try {
+            return values.getInt(key);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public int getShort(String key) {
+        try {
+            return values.getInt(key);
+
+        } catch (JSONException e) {
+            return 0;
+        }
+    }
+
+    public String getString(String key) {
+        try {
+            return values.getString(key);
+
+        } catch (JSONException e) {
+            return "";
+        }
+    }
+
 
     //get JsonObject
     public JSONObject getRoot() {
+
+        //se è stato creato l'oggetto nested lo inserisco
+        if (nestedObjectExists) {
+            try {
+                root.put(Jack.JK_MESSAGE_PAYLOAD, values);
+
+            } catch (JSONException e){
+                Logger.e("JData", "getRoot", e, Logger.SEVERE);
+            }
+        }
 
         //ritorno la root del messaggio
         return root;
@@ -80,7 +296,15 @@ public class JData {
 
     //---PRIVATE---
 
+    public void createNestedObject() {
 
+        //creo l'oggetto nested
+        values = new JSONObject();
+
+        //indico che è stato creato l'oggetto nested
+        nestedObjectExists = true;
+
+    }
 
 
 }
