2f17195b4d8650c3a2daa487281b1ac612016b4e
Alessandro Pasqualini <alessandro.pasqualini.1105@gmail.com>
2016-05-22 19:40:06 +0200

    Sistemata creazione SettingsBandFragment con lo stato di connessione al band

--- app/src/main/java/com/lewetechnologies/app/activities/BandSettingsActivity.java
+++ app/src/main/java/com/lewetechnologies/app/activities/BandSettingsActivity.java
@@ -1,18 +1,29 @@
 package com.lewetechnologies.app.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.lewetechnologies.app.configs.Config;
 import com.lewetechnologies.app.fragments.SettingsBandPreferenceFragment;
 
 public class BandSettingsActivity extends AppCompatActivity {
+
+    //shared preference
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Display the fragment as the main content.
-        getFragmentManager().beginTransaction().replace(android.R.id.content, SettingsBandPreferenceFragment.newInstance(false)).commit();
+        //prelevo le shared preference
+        preferences = getApplicationContext().getSharedPreferences(Config.SHARED_PREFERENCE_FILE, Context.MODE_PRIVATE);
+
+        //Creo il fragment passando lo stato della connessione con il band
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+            SettingsBandPreferenceFragment.newInstance(preferences.getBoolean(Config.SHARED_PREFERENCE_KEY_BAND_STATUS, false)))
+            .commit();
 
     }
 }
